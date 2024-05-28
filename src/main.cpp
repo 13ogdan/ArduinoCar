5bf50d093166d087cffe4823e7097b511dbca5aa
Bohdan Spilnyi <bogdanspilnyy@gmail.com>
2024-05-28 19:55:08 +0300

    Lesson2

--- src/main.cpp
+++ src/main.cpp
@@ -3,25 +3,14 @@
 void setup()
 { 
   pinMode(9, OUTPUT);// initialize digital pin 9 as an output.
-
-  Serial.print("Hello, World!\r\nArduino Forever");
 }
 
 void loop() // the loop function runs over and over again forever
 { 
-  Serial.begin(9600);
-
-  Serial.println("turn the LED on (HIGH is the voltage level)");
-  digitalWrite(9, HIGH); // turn the LED on (HIGH is the voltage level)
+  for (int i = 0; i<255; i++)
+  {
+      analogWrite(9, i);
+      delay(100);
+  }
   
-  Serial.println("wait for a second");
-  delay(3000); // wait for a second
-
-  Serial.println("turn the LED off by making the voltage LOW");
-  digitalWrite(9, LOW); // turn the LED off by making the voltage LOW
-  
-  Serial.println("wait for a second");
-  delay(1000); // wait for a second
-
-  Serial.end();
 }
