38333dea2bb2acb3a26f1022ba46e257e63078d3
Bohdan Spilnyi <bogdanspilnyy@gmail.com>
2024-06-02 16:12:05 +0300

    test commmit

--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,49 @@
 #include <Sonar.h>
 #include <IRremote.h>
 
-Console* console = new Console();
+#define DEBUG
+#define CUSTOM_SETTINGS
+#define INCLUDE_TERMINAL_MODULE
+#include <Dabble.h>
+
+String Serialdata = "";
+bool dataflag = 0;
+long timestamp = 0;
 
 void setup()
 {
-  Serial.begin(9600);
-  Serial.println("<Arduino is ready>");
-  console->SetCommandEnding(";");
+  Serial.begin(250000); // make sure your Serial Monitor is also set at this baud rate.
+  Dabble.begin(9600); // Change this baudrate as per your bluetooth baudrate. Connect bluetooth on digital pin 2(RX) and 3(TX) for Uno/Nano and on Serial3 pins for Mega.
 }
 
 void loop()
 {
-  String command = console -> ReadCommand();
-  if (command != EMPTY)
+  Dabble.processInput();
+  long newTimestamp = millis();
+  if (newTimestamp - timestamp > 2000)
   {
-    Serial.println("New command: " + command);
-    if (command == "путін")
-        Serial.println("=Хуйло");
+    
+    timestamp = newTimestamp;
+    String value = "";
+
+    value = "Is initialized: " +  String(Dabble.isInitialized());
+    value += "\t isAppConnected:" + String(Dabble.isAppConnected());
+    value += "\t isSoftwareSerial:" + String(Dabble.isSoftwareSerial());
+    value += "\t DabbleSerial:" + String(Dabble.DabbleSerial->available());
+    
+    Serial.println(value);
   }
-   delay(100);  
+
+  while (Serial.available())
+  {
+    int newChar = Serial.read();
+    Terminal.write(newChar);
+  }
+
+  while (Terminal.available())
+  {
+    int newChar = Terminal.read();
+    Serial.write(newChar);
+  }
+  
 }
