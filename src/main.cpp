0e58249b2002ce2e5ef7433e9d1415e0c3400855
Bohdan Spilnyi <bogdanspilnyy@gmail.com>
2024-06-03 20:49:40 +0300

    Set 90 angle

--- src/main.cpp
+++ src/main.cpp
@@ -13,21 +13,13 @@ void setup()
 {
   Serial.begin(9600);
   Serial.println("<Arduino is ready>");
-  console = Console();
 
-  servo.attach(A3, 0000, 2500);
-  servo.write(0);
+  servo.attach(A3);
+  servo.write(90);
 
 }
 
 void loop()
 {
-  String command = console.ReadCommand();
-  if (command != EMPTY)
-  {
-    int angle = command.toInt();
-    Serial.print("Send angle:");
-    Serial.println(angle);
-    servo.write(angle);
-  }
+
 }
